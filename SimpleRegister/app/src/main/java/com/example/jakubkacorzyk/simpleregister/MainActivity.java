acbeb6f8a1ea398c9812f4b1ca691493c6353877
Bartłomiej Łazarczyk <lazar81ba@gmail.com>
2018-11-28 21:25:48 +0100

    Added toolbar with two icons

--- SimpleRegister/app/src/main/java/com/example/jakubkacorzyk/simpleregister/MainActivity.java
+++ SimpleRegister/app/src/main/java/com/example/jakubkacorzyk/simpleregister/MainActivity.java
@@ -5,11 +5,15 @@ import android.provider.MediaStore;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -19,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
         Button register = (Button)findViewById(R.id.register);
         RadioButton male = (RadioButton)findViewById(R.id.male);
         male.setChecked(true);
@@ -42,5 +47,38 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        ((Toolbar) findViewById(R.id.toolbar)).setOnMenuItemClickListener(
+                new Toolbar.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        return onOptionsItemSelected(item);
+                    }
+                });
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.launcher:
+                Toast.makeText(this, "Launcher selected", Toast.LENGTH_SHORT)
+                        .show();
+                break;
+            case R.id.launcher_round:
+                Toast.makeText(this, "LauncherRound selected", Toast.LENGTH_SHORT)
+                        .show();
+                break;
+            default:
+                break;
+        }
+
+        return true;
     }
 }
